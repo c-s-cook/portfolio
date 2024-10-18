e821e9d0b4450b003d680146096041e0a9617841
Christopher Cook <christopherstephencook@gmail.com>
2024-10-18 08:52:38 -0500

    added TS + PreviewCard desktop all workds (missing responsive)

---
+++ frontend/app/components/PreviewCard/PreviewCard.tsx
@@ -0,0 +1,159 @@
+"use client"
+
+import Image from "next/image";
+import Link from "next/link";
+import "./PreviewCard.css";
+import { ReactElement, useEffect, useRef } from "react";
+
+export interface Project {
+    title: string,
+    url: string,
+    thumbnails: Array<object>,
+    snippet: string,
+    tags: Array<string>,
+};
+
+export interface Certification extends Project {
+    date: Date,
+}
+
+export interface PreviewCardProps {
+    cardID: number,
+    project: Project | Certification,
+    interval: number
+}
+
+
+// Creating a default Project prop for dev purposes
+const defaultProject: Project = {
+    title: "This is a Demo Title",
+    url: "/",
+    thumbnails: [
+        {
+            src: "/img/IMG-20210110-WA0000.jpg",
+            alt: "Picture of the author & child at play"
+        },
+        {
+            src: "/img/IMG-20210110-WA0001.jpg",
+            alt: "2nd pic of the author & child at play"
+        },
+        {
+            src: "/img/IMG-20210110-WA0002.jpg",
+            alt: "3rd photo of the author & child at play"
+        },
+    ],
+    snippet: "This is a snippet of text that should get cut off if there is too much and too lengthing a run of words here. Hopefully I can use a cut-off item for a read more element.",
+    tags: [ "React", "MongoDB", "Kubernetes", "Node.js" ]
+}
+
+/**
+ * 
+ * @param   {number}                cardID      Unique. Acts as a key for distinguishing each card in a collection of cards.
+ * @param   {Project | Credential}  project     All the necessary info for the compontent, as defined by the interface (..do I need to write this?)
+ * @param   {number}                interval    Optional. In seconds, how quickly the thumbnails should rotate. DEFAULT: 2.5
+ * @returns {ReactNode}
+ */
+
+
+const PreviewCard = ({ cardID, project, interval = 2.5 }: PreviewCardProps): ReactElement => {
+
+    const ref = useRef(null);
+    
+
+    const shuffleThumbs = (card: HTMLBodyElement) => {
+        
+
+        let thumbs:Element = card.getElementsByClassName("thumbnails")[0];
+
+        // Attempting Re-ordering...
+        if (thumbs.firstChild.style.left != "0%"){
+            let reordered = thumbs.firstChild;
+            reordered.style.zIndex = "-1";
+            thumbs.removeChild(thumbs.firstChild);
+            thumbs.appendChild(reordered);
+            thumbs.lastChild.style.left = `${(thumbs.children.length - 1) * 100}%`;            
+        }
+
+        // Sliding the thumbnails over...
+        for(let i=0; i < thumbs.children.length; i++){
+            thumbs.children[i].style.zIndex = "0";
+            thumbs.children[i].style.left = `${(i-1) * 100}%`;
+        }
+    }
+
+
+    useEffect(() => {
+
+        let shuffleInterval;
+
+        const startShuffle = (e: UIEvent) => {
+            e.preventDefault();  
+            shuffleThumbs(e.target);
+            
+            if(!shuffleInterval){ 
+                shuffleInterval = setInterval(shuffleThumbs, interval*1000, e.target); 
+            }
+        }
+    
+        const stopShuffle = (e: UIEvent) => {
+            e.preventDefault();
+            
+            clearInterval(shuffleInterval);
+            shuffleInterval = null;    
+        }
+
+        const card = ref.current;
+
+        card.addEventListener('mouseenter', startShuffle);
+        card.addEventListener('mouseleave', stopShuffle, false);
+
+
+    }, [])
+    
+    project = !project ? defaultProject : project;
+    
+    return ( 
+        <>
+        <Link href="/">
+        <div ref={ref} className="preview-card with-background" id={"preview-card-" + cardID}>
+        <div className="thumbnails">
+                { project.thumbnails.map((img, index) => {
+                    return (
+                        <Image
+                            src={img.src}
+                            width={500}
+                            height={500}
+                            alt={img.alt}
+                            key={index}
+                            style={{
+                                zIndex: `${(index*-1)}`,
+                                left: `${index*100}%`
+                            }}
+                        />
+                    )
+
+                })}
+
+            </div>
+            
+            <h3>{project.title}</h3>
+            <div className="preview-snippet">
+                <p >{project.snippet}</p>
+            </div>
+            <div className="tags">
+                <ul>
+                    { project.tags.map((tag, index) => {
+                        return <li key={index}>{tag}</li>
+                    })}
+
+                </ul>
+            </div>  
+            <button>READ MORE</button>
+            
+        </div>
+        </Link>
+        </>
+     );
+}
+ 
+export default PreviewCard;
--- frontend/app/page.js
+++ frontend/app/page.js
@@ -7,13 +7,14 @@ import './styles.css'
 
 import { Satisfy } from 'next/font/google'
 import Logo from './components/Logo/Logo'
+import PreviewCard from './components/PreviewCard/PreviewCard'
 const satisfy = Satisfy({ subsets: ['latin'], weight: ['400'] })
 
 
 export default function Home() {
 
 
-
+  let prvi=1;
 
 
 
@@ -30,11 +31,12 @@ export default function Home() {
             <Logo />
           </div>
         </section>
+
         <section>
-          <div className="content">
-            <div className="section-title">
-              <h1 className={satisfy.className}>About me...</h1>
-            </div>
+          <div className="content with-background">
+        
+            <h2 className={satisfy.className}>About me...</h2>
+      
             
             <div className="body">
             <p>
@@ -45,53 +47,55 @@ export default function Home() {
         </section>
 
         <section>
-          <div className="content long">
-            <div className="section-title">
-              <h1 className={satisfy.className}>About me 2...</h1>
-            </div>
+          <div className="content extra-long">
             
-            <div className="body">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pellentesque dignissim enim sit amet venenatis urna. In fermentum et sollicitudin ac orci phasellus egestas tellus. Cras semper auctor neque vitae. Tincidunt vitae semper quis lectus nulla at volutpat diam. Pellentesque diam volutpat commodo sed egestas egestas fringilla phasellus. Vel pharetra vel turpis nunc eget lorem. Sodales ut eu sem integer vitae justo eget magna. Egestas erat imperdiet sed euismod nisi porta lorem. Dui accumsan sit amet nulla facilisi. Mattis vulputate enim nulla aliquet porttitor lacus luctus accumsan. Turpis in eu mi bibendum neque egestas congue. Suspendisse interdum consectetur libero id faucibus nisl tincidunt.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pellentesque dignissim enim sit amet venenatis urna. In fermentum et sollicitudin ac orci phasellus egestas tellus. Cras semper auctor neque vitae. Tincidunt vitae semper quis lectus nulla at volutpat diam. Pellentesque diam volutpat commodo sed egestas egestas fringilla phasellus. Vel pharetra vel turpis nunc eget lorem. Sodales ut eu sem integer vitae justo eget magna. Egestas erat imperdiet sed euismod nisi porta lorem. Dui accumsan sit amet nulla facilisi. Mattis vulputate enim nulla aliquet porttitor lacus luctus accumsan. Turpis in eu mi bibendum neque egestas congue. Suspendisse interdum consectetur libero id faucibus nisl tincidunt.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pellentesque dignissim enim sit amet venenatis urna. In fermentum et sollicitudin ac orci phasellus egestas tellus. Cras semper auctor neque vitae. Tincidunt vitae semper quis lectus nulla at volutpat diam. Pellentesque diam volutpat commodo sed egestas egestas fringilla phasellus. Vel pharetra vel turpis nunc eget lorem. Sodales ut eu sem integer vitae justo eget magna. Egestas erat imperdiet sed euismod nisi porta lorem. Dui accumsan sit amet nulla facilisi. Mattis vulputate enim nulla aliquet porttitor lacus luctus accumsan. Turpis in eu mi bibendum neque egestas congue. Suspendisse interdum consectetur libero id faucibus nisl tincidunt.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pellentesque dignissim enim sit amet venenatis urna. In fermentum et sollicitudin ac orci phasellus egestas tellus. Cras semper auctor neque vitae. Tincidunt vitae semper quis lectus nulla at volutpat diam. Pellentesque diam volutpat commodo sed egestas egestas fringilla phasellus. Vel pharetra vel turpis nunc eget lorem. Sodales ut eu sem integer vitae justo eget magna. Egestas erat imperdiet sed euismod nisi porta lorem. Dui accumsan sit amet nulla facilisi. Mattis vulputate enim nulla aliquet porttitor lacus luctus accumsan. Turpis in eu mi bibendum neque egestas congue. Suspendisse interdum consectetur libero id faucibus nisl tincidunt.
-            </p>
+            <h2 className={satisfy.className}>About me 2...</h2>
+           
+            
+            <div className="body preview-deck">
+            
+            
+
+
+            
+              <PreviewCard cardID={1} />
+              <PreviewCard cardID={2} />
+              <PreviewCard cardID={3} />
+              <PreviewCard cardID={4} />
+              <PreviewCard cardID={5} />
+              <PreviewCard cardID={6} />
+
+            
+
             </div>
           </div>
         </section>
 
         <section>
           <div className="content">
-            <div className="section-title">
-              <h1 className={satisfy.className}>About me 3...</h1>
-            </div>
+            
+            <h2 className={satisfy.className}>About me 3...</h2>
             
             <div className="body">
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pellentesque dignissim enim sit amet venenatis urna. In fermentum et sollicitudin ac orci phasellus egestas tellus. Cras semper auctor neque vitae. Tincidunt vitae semper quis lectus nulla at volutpat diam. Pellentesque diam volutpat commodo sed egestas egestas fringilla phasellus. Vel pharetra vel turpis nunc eget lorem. Sodales ut eu sem integer vitae justo eget magna. Egestas erat imperdiet sed euismod nisi porta lorem. Dui accumsan sit amet nulla facilisi. Mattis vulputate enim nulla aliquet porttitor lacus luctus accumsan. Turpis in eu mi bibendum neque egestas congue. Suspendisse interdum consectetur libero id faucibus nisl tincidunt.
             </p>
             </div>
+
           </div>
         </section>
 
         <section>
           <div className="content">
-            <div className="section-title">
-              <h1 className={satisfy.className}>About me 4...</h1>
-            </div>
             
+            <h2 className={satisfy.className}>About me 4...</h2>
+                        
             <div className="body">
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pellentesque dignissim enim sit amet venenatis urna. In fermentum et sollicitudin ac orci phasellus egestas tellus. Cras semper auctor neque vitae. Tincidunt vitae semper quis lectus nulla at volutpat diam. Pellentesque diam volutpat commodo sed egestas egestas fringilla phasellus. Vel pharetra vel turpis nunc eget lorem. Sodales ut eu sem integer vitae justo eget magna. Egestas erat imperdiet sed euismod nisi porta lorem. Dui accumsan sit amet nulla facilisi. Mattis vulputate enim nulla aliquet porttitor lacus luctus accumsan. Turpis in eu mi bibendum neque egestas congue. Suspendisse interdum consectetur libero id faucibus nisl tincidunt.
             </p>
             </div>
+            
           </div>
         </section>
 
