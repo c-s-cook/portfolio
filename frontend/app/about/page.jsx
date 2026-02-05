6938554a45f8193efcbf5e4b2b1e9a14962d3547
Christopher Cook <christopherstephencook@gmail.com>
2026-02-05 16:03:48 -0600

    removed tailwind

--- frontend/app/about/page.jsx
+++ frontend/app/about/page.jsx
@@ -5,7 +5,7 @@
 export default function About() {
   return (
     <>
-        <div className="mx-auto text-center">
+        <div className="">
             <h1>About Me</h1>
             <p>Here is a bit about me...</p>
         </div>
